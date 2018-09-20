1e9ba6c8992ada4887d508971b4f24c0e6a5eeca
Torge Matthies <openglfreak@googlemail.com>
2018-09-20 22:01:30 +0200

    Import public functions in __init__.py so the package is usable like a module.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
-__all__ = (
-	'classes',
-	'parse_xrandr'
-)
+from . import classes
+from . import parse_xrandr
+
+__all__ = classes.__all__ + parse_xrandr.__all__
+
+from .classes import *
+from .parse_xrandr import *
