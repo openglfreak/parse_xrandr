18d7050deb2855a08552915f0628d42ad801329d
Torge Matthies <openglfreak@googlemail.com>
2018-09-21 19:15:22 +0200

    Fixed wrong module names in __init__.py.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from . import classes
-from . import parse_xrandr
+from . import parse
 
-__all__ = classes.__all__ + parse_xrandr.__all__
+__all__ = classes.__all__ + parse.__all__
 
 from .classes import *
 from .parse import *
