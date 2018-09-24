110f66a21485e660e1b9d5efaac50b7ec125bda0
Torge Matthies <openglfreak@googlemail.com>
2018-09-24 15:37:31 +0200

    Renamed parse_xrandr_screens to parse_screens.

--- parse.py
+++ parse.py
@@ -5,21 +5,9 @@ from .parser import *
 from .parsing import *
 
 __all__ = (
-	'parse_xrandr_screens',
-	'parse_xrandr'
+	'parse_xrandr',
+	'parse_screens'
 )
-
-# Parsing helpers
-
-def parse_xrandr_screens(xrandr_output, start=0):
-	xrandr_output, start, screens, matches = parse(
-		xrandr_output,
-		start,
-		{},
-		((screen_regex, screen_func),)
-	)
-	success = start == len(xrandr_output)
-	return screens, success
 
 def parse_xrandr():
 	xrandr_path = which('xrandr')
@@ -29,4 +17,14 @@ def parse_xrandr():
 	with Popen(('xrandr', '--verbose'), executable=xrandr_path, stdout=PIPE, universal_newlines=True) as popen:
 		xrandr_output = popen.stdout.read()
 	
-	return parse_xrandr_screens(xrandr_output)
+	return parse_screens(xrandr_output)
+
+def parse_screens(xrandr_output, start=0):
+	xrandr_output, start, screens, matches = parse(
+		xrandr_output,
+		start,
+		{},
+		((screen_regex, screen_func),)
+	)
+	success = start == len(xrandr_output)
+	return screens, success
